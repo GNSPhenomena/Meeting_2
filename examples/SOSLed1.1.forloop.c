317168f4cd993bbe642118d7a36e9fadb95bcd6b
Albert <32423607+GNSPhenomena@users.noreply.github.com>
2017-09-30 18:14:01 -0700

    Update SOSLed1.1.forloop.c

--- examples/SOSLed1.1.forloop.c
+++ examples/SOSLed1.1.forloop.c
@@ -11,36 +11,42 @@ void setup(){
 
 void loop(){
   	//------------ RED ------------
-	for(int i=0; i<3; i++)
+	for(int i=0; i<3; i++) // repeat 3 times
 	{
+		// Turn on LED1, and turn off others
 		digitalWrite(ledPin1, HIGH);
 		digitalWrite(ledPin2, LOW);
 		digitalWrite(ledPin3, LOW);
 		delay(1000);
+		// Turn off all LEDs
 		digitalWrite(ledPin1, LOW);
 		digitalWrite(ledPin2, LOW);
 		digitalWrite(ledPin3, LOW);
 		delay(1000);
 	}
 	//------------ GREEN ------------
-	for(int j=0; j<3; j++)
+	for(int j=0; j<3; j++) // repeat 3 times
 	{
+		// Turn on LED2, and turn off others
 		digitalWrite(ledPin1, LOW);
 		digitalWrite(ledPin2, HIGH);
 		digitalWrite(ledPin3, LOW);
 		delay(2000);
+		// Turn off all LEDs
 		digitalWrite(ledPin1, LOW);
 		digitalWrite(ledPin2, LOW);
 		digitalWrite(ledPin3, LOW);
 		delay(2000);
 	}
 	//------------ BLUE ------------
-	for(int k=0; k<3; k++)
+	for(int k=0; k<3; k++) // repeat 3 times
 	{
+		// Turn on LED3, and turn off others
 		digitalWrite(ledPin1, LOW);
 		digitalWrite(ledPin2, LOW);
 		digitalWrite(ledPin3, HIGH);
 		delay(1000);
+		// Turn off all LEDs
 		digitalWrite(ledPin1, LOW);
 		digitalWrite(ledPin2, LOW);
 		digitalWrite(ledPin3, LOW);
